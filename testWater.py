a770c08c96a48e924d45965a4e4d27af72e25db3
Kirisanth Ganesh <kirisanth.g@hotmail.com>
2018-09-06 14:51:49 -0400

    Added more tests

--- testWater.py
+++ testWater.py
@@ -27,3 +27,59 @@ class TestSolver(unittest.TestCase):
         actual = water.solve(input)
         self.assertEqual(expected_cost, actual['Cost'])
         self.assertEqual(expected_pipes, set(actual['Pipes']))
+
+    def testTwoPipe(self):
+        input = { 'truck': 10, 'pipes': [{ 'name': 'pipe 1', 'cost': 1, 'litres': 4 }, { 'name': 'pipe 2', 'cost': 15, 'litres': 6}] }
+        expected_pipes = set(['pipe 1', 'pipe 2'])
+        expected_cost = 16
+        actual = water.solve(input)
+        self.assertEqual(expected_cost, actual['Cost'])
+        self.assertEqual(expected_pipes, set(actual['Pipes']))
+
+    def testMulitplePossible(self):
+        input = { 'truck': 10, 'pipes': [{ 'name': 'pipe 1', 'cost': 1, 'litres': 10 }, { 'name': 'pipe 2', 'cost': 1, 'litres': 10}] }
+        expected_pipes = set(['pipe 1', 'pipe 2'])
+        expected_cost = 1
+        actual = water.solve(input)
+        self.assertEqual(expected_cost, actual['Cost'])
+        self.assertTrue(set(actual['Pipes']).issubset(expected_pipes))
+
+    def testOnePipeNotPossible(self):
+        input = { 'truck': 10, 'pipes': [ { 'name': 'pipe 1', 'cost': 14, 'litres': 20 }] }
+        expected_pipes = set([])
+        expected_cost = 0
+        actual = water.solve(input)
+        self.assertEqual(expected_cost, actual['Cost'])
+        self.assertEqual(expected_pipes, set(actual['Pipes']))
+
+    def testMultiPipeNotPossible(self):
+        input = { 'truck': 10, 'pipes': [ { 'name': 'pipe 3', 'cost': 4, 'litres': 20 }, { 'name': 'pipe 4', 'cost': 11, 'litres': 30 }, { 'name': 'pipe 1', 'cost': 1, 'litres': 11 }, { 'name': 'pipe 2', 'cost': 15, 'litres': 26} ] }
+        expected_pipes = set([])
+        expected_cost = 0
+        actual = water.solve(input)
+        self.assertEqual(expected_cost, actual['Cost'])
+        self.assertEqual(expected_pipes, set(actual['Pipes']))
+
+    def testNoPipes(self):
+        input = { 'truck': 10, 'pipes': [] }
+        expected_pipes = set([])
+        expected_cost = 0
+        actual = water.solve(input)
+        self.assertEqual(expected_cost, actual['Cost'])
+        self.assertEqual(expected_pipes, set(actual['Pipes']))
+
+    def testZeroLimit(self):
+        input = { 'truck': 0, 'pipes': [{ 'name': 'pipe 1', 'cost': 1, 'litres': 4 }, { 'name': 'pipe 2', 'cost': 15, 'litres': 6}] }
+        expected_pipes = set([])
+        expected_cost = 0
+        actual = water.solve(input)
+        self.assertEqual(expected_cost, actual['Cost'])
+        self.assertEqual(expected_pipes, set(actual['Pipes']))
+
+    def testNoPipesZeroLimit(self):
+        input = { 'truck': 0, 'pipes': [] }
+        expected_pipes = set([])
+        expected_cost = 0
+        actual = water.solve(input)
+        self.assertEqual(expected_cost, actual['Cost'])
+        self.assertEqual(expected_pipes, set(actual['Pipes']))
